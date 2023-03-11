74d685c8f833f838a4f8e26190c016e5cac5a01c
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2023-03-10 20:33:28 -0500

    Add: Tailwind CSS

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className=''>
+      <main className='min-h-screen'>
         <h1>Hello</h1>
       </main>
     </div>
