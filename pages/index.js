337d34e4b4543ca9c07b828ac86ef47c402c36cd
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2023-03-10 21:03:10 -0500

    Add: Base Markup

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,32 @@ export default function Home() {
       title='uninot | Share with the world'
       description='This app allows you to share your thoughts easily'
     >
-      <h1>Hello</h1>
+      <section className='max-w-screen-md mx-auto pt-10'>
+        <form className='flex shadow-lg border'>
+          <input
+            className='p-2 flex-1 outline-none'
+            type='text'
+            placeholder='What are you thinking???'
+          />
+          <button className='p-2 px-4 bg-blue-500 hover:bg-blue-600 text-white'>
+            Share
+          </button>
+        </form>
+        <ul>
+          <li className='shadow-lg border bg-white my-4 p-4'>
+            <h2 className='text-lg'>uninot</h2>
+          </li>
+          <li className='shadow-lg border bg-white my-4 p-4'>
+            <h2 className='text-lg'>uninot</h2>
+          </li>
+          <li className='shadow-lg border bg-white my-4 p-4'>
+            <h2 className='text-lg'>uninot</h2>
+          </li>
+          <li className='shadow-lg border bg-white my-4 p-4'>
+            <h2 className='text-lg'>uninot</h2>
+          </li>
+        </ul>
+      </section>
     </Layout>
   );
 }
